93d9c438dea5160b7e90d51fb918fdf11cd21f97
Hima Aramona <19400811+HimaAramona@users.noreply.github.com>
2026-04-10 17:04:52 -0700

    chore: make GrainProfile.viewer optional

--- Grain/Models/Views/ProfileModels.swift
+++ Grain/Models/Views/ProfileModels.swift
@@ -10,6 +10,7 @@ struct GrainProfile: Codable, Sendable, Identifiable {
     var labels: [ATLabel]?
     var avatar: String?
     var createdAt: String?
+    var viewer: ActorViewerState?
 
     var id: String {
         did
